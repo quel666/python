607a01997e087835590422f9c4d677d9b16f8ccb
quel666 <168922095+quel666@users.noreply.github.com>
2024-05-04 12:57:13 -0300

    Add files via upload

---
+++ exercicio01.py
@@ -0,0 +1,11 @@
+#curso basico de python
+#nome do desevolvedor: Raquel Ferreira Rocha
+#versão 1.0
+#exercicio de logica de programação 
+#com a linguagem de programação python
+#exercicio de soma
+print ("programa soma")
+numero1= int (input("digite um valor "))
+numero2 = int(input ("digite um numero "))
+soma= numero1 + numero2 
+print ("A soma dos numeros digitados é", soma)
---
+++ exercicio04.py
@@ -0,0 +1,11 @@
+#curso basico de python
+#nome do desevolvedor: Raquel Ferreira Rocha
+#versão 1.0
+#exercicio de logica de programação 
+#com a linguagem de programação python
+#exercicio de divisao
+print ("programa divisão")
+numero1= int (input("digite um valor "))
+numero2 = int(input ("digite um numero "))
+divisao= numero1 / numero2 
+print ("A soma dos numeros digitados é", divisao)
