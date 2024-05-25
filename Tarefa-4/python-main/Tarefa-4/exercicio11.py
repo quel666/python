933d81145145f1fccbde336891a0a75dc1b8fdf3
quel666 <168922095+quel666@users.noreply.github.com>
2024-05-25 06:46:11 -0300

    Add files via upload

---
+++ Tarefa-4/python-main/Tarefa-4/exercicio11.py
@@ -0,0 +1,11 @@
+
+macas = int (input("Quantas maçãs você gostaria?: "))
+
+valor1 = 1.30
+valor2 = 1.00
+
+if macas <= 12:
+    print ("As maçãs custaram: ", macas * valor1)
+
+else: 
+    print ("As maçãs custaram: ", macas * valor2)
---
+++ Tarefa-4/python-main/Tarefa-5/exercicio1.py
@@ -0,0 +1,4 @@
+total_de_eleitores = int (input("Digite o total de eleitores do município:"))
+numero_de_votos_brancos = int (input("Digite o número de votos brancos:"))
+votos_nulos = int (input("Digite o número de votos nulos:"))
+votos_validos = int (input("Digite o número de votos validos:"))
